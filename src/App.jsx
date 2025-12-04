419291714472c880fd6c1b97fb9e0f8e384871fa
ivan <ivanlares1@gmail.com>
2025-12-04 10:24:25 -0800

    Add react-router-dom dependency

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Containers/Home/Home";
 import Login from "./Containers/Login/Login";
+import './App.css';
 
 function App() {
   return (
     <div>
       <Login />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
