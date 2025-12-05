6795c1bcca3f79977ce28c5dd3de642dfd98658b
ivan <ivanlares1@gmail.com>
2025-12-05 11:47:17 -0800

    Add contentDiv in Home Container

--- src/Containers/Home/Home.jsx
+++ src/Containers/Home/Home.jsx
@@ -6,6 +6,11 @@ const Home = () => {
     return (
         <div id="mainDiv">
             <Header/>
+            <div id="contentDiv">
+                <p>Hello world</p>
+                <br></br>
+                <p>Hello world!</p>
+            </div>
         </div>
     )
 };
