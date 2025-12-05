aa981f8b57a0287a462414a32044d016739311cd
ivan <ivanlares1@gmail.com>
2025-12-04 19:31:14 -0800

    Finalize Login Container

--- src/Containers/Login/Login.jsx
+++ src/Containers/Login/Login.jsx
@@ -4,8 +4,20 @@ const Login = () => {
 
     return (
         <div id="MainDiv">
-            <div id="HeaderDiv">Profiles</div>
-            <div id="LoginDiv">Username</div>
+            <div id="HeaderDiv">
+                <p id="HeaderText">Profiles</p>
+                <p id="SloganText">A simple & lightweight social media app.</p>
+            </div>
+            <br/> <br/>
+            <div id="LoginDiv">
+                <br/>
+                <input id="UsernameInput" type="text" placeholder="Username"/> 
+                <br/>
+                <input id="PasswordInput" type="password" placeholder="Password"/> 
+                <br/>
+                <button id="LoginButton" type="button">Log In</button>
+                <br/>
+            </div>
         </div>
     )
 };
