39f4d1e37c512d1d5c7c803d6c67a06d7f2403f3
ivan <ivanlares1@gmail.com>
2025-12-05 22:07:28 -0800

    Show profile data in EditProfile Container

--- src/Containers/EditProfile/EditProfile.jsx
+++ src/Containers/EditProfile/EditProfile.jsx
@@ -1,8 +1,28 @@
 import "./EditProfile.css"
 import Header from "../../Component/Header/Header.jsx"
 import userAvatar from "../../Assets/UserImages/user-avatar.svg"
+import { ProfilesContext } from "../../Context/ProfilesContext.jsx"
+import { useContext, useEffect, useRef, useState } from "react";
 
 const EditProfile = () => {
+
+    const { getCurrentUsername, getProfileData } = useContext(ProfilesContext);
+    const [photoURL, setPhotoURL] = useState('');
+    const [aboutMe, setAboutMe] = useState('');
+    const [linkedInURL, setLinkedInURL] = useState('');
+    const [instagramURL, setInstagramURL] = useState('');
+    const [spotifyEmbed, setSpotifyEmbed] = useState('');
+
+    useEffect( () => {
+        let profileData = getProfileData(getCurrentUsername())
+        if (profileData != null) {
+            setPhotoURL(profileData.photoURL ?? '');
+            setAboutMe(profileData.aboutMe ?? '');
+            setLinkedInURL(profileData.linkedInURL ?? '');
+            setInstagramURL(profileData.instagramURL ?? '');
+            setSpotifyEmbed(profileData.spotifyEmbed ?? '');
+        }
+    }, [])
 
     return (
         <div id="editProfileRootDiv">
@@ -10,23 +30,23 @@ const EditProfile = () => {
             <div className="contentDiv">
                 <div className="editProfileItem">
                     <img className="profile-icon-medium" src={userAvatar}></img>
-                    <input className="edit-profile-input" type="url" placeholder="Photo URL"></input>
+                    <input className="edit-profile-input" type="url" placeholder="Photo URL" defaultValue={photoURL}></input>
                 </div>
 
                 <div className="editProfileItem">
-                    <textarea id="edit-profile-about-input" type="text" placeholder="About me"></textarea>
+                    <textarea id="edit-profile-about-input" type="text" placeholder="About me" defaultValue={aboutMe}></textarea>
                 </div>
 
                 <div className="editProfileItem">
-                    <input className="edit-profile-input" type="url" placeholder="LinkedIn"></input>
+                    <input className="edit-profile-input" type="url" placeholder="LinkedIn" defaultValue={linkedInURL}></input>
                 </div>
 
                 <div className="editProfileItem">
-                    <input className="edit-profile-input" type="url" placeholder="Instagram"></input>
+                    <input className="edit-profile-input" type="url" placeholder="Instagram" defaultValue={instagramURL}></input>
                 </div>
 
                 <div className="editProfileItem">
-                    <textarea className="edit-profile-embed" type="url" placeholder="Spotify Embed"></textarea>
+                    <textarea className="edit-profile-embed" type="url" placeholder="Spotify Embed" defaultValue={spotifyEmbed}></textarea>
                 </div>
             </div>
         </div>
