ee002afd321d67973107ea1d6ed39ad43dc0992e
ivan <ivanlares1@gmail.com>
2025-12-06 00:00:34 -0800

    Update InitialData
    add tom anderson

--- src/Data/InitialData.jsx
+++ src/Data/InitialData.jsx
@@ -1,4 +1,5 @@
 import obamaImageURL from "../Assets/UserImages/obama.png";
+import tomImageURL from "../Assets/UserImages/tom.jpg";
 
 export const initialUserCredentials = {
     "ivan": {
@@ -11,6 +12,10 @@ export const initialUserCredentials = {
     },
     "obama": {
         "username": "obama",
+        "password": "123"
+    },
+    "tom": {
+        "username": "tom",
         "password": "123"
     }
 }
@@ -32,5 +37,9 @@ export const initialUserProfiles = {
         "linkedInURL": "https://www.linkedin.com/in/barackobama",
         "instagramURL": "https://www.instagram.com/barackobama/?hl=en",
         "spotifyEmbed": `<iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/playlist/3xN6J0LCyVj8k1gVCguWRH?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`
+    },
+    "tom": {
+        "photoURL": tomImageURL,
+        "aboutMe": "Thomas Anderson is an American technology entrepreneur and co-founder of the social networking website Myspace, which he founded in 2003 with Chris DeWolfe. He was later president of Myspace and a strategic adviser for the company"
     }
 }
